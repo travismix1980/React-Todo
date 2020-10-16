b91db60a67d024a7f65236991cd5646f4ac79921
Travis Mix <travismix1980@gmail.com>
2020-10-16 15:54:53 -0700

    Main App done adding router now to get JSON placeholder data to create our todos to start with

---
+++ src/components/todos/TodoItem.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component{
+    getStyle = () =>{
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+    render(){
+        const {id, title} = this.props.todo;
+        return(
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/>{" "}
+                    {title}
+                    <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>x</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+}
+
+const btnStyle = {
+    backgroundColor: 'red',
+    color: 'white',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right',
+}
+
+export default TodoItem;
